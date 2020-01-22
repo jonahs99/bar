854aebcdbe824d36a26ca97aa1c09262d1096999
Zak Simpson <zakjsimpson@gmail.com>
2020-01-22 15:50:07 -0500

    Change workspaces when one is clicked

--- wks.py
+++ wks.py
@@ -3,12 +3,15 @@ import fmt
 import icons
 
 def fmt_symbol(s):
-    wksname = ' {} '.format(s[1:])
+    w = ' {} '.format(s[1:])
+    link = 'bspc desktop -f {}'.format(s[1:])
     if s[0] in 'FOU':
-        return fmt.und(fmt.clr(wksname, 15), 2)
+        w = fmt.und(fmt.clr(w, 15), 2)
     elif s[0] in 'ou':
-        return fmt.clr(wksname, 15)
-    return fmt.clr(wksname, 8)
+        w = fmt.clr(w, 15)
+    else:
+        w = fmt.clr(w, 8)
+    return fmt.cmd(w, link)
 
 def get_wks(bspc_output):
     items = [item for item in bspc_output.split(':') if item[0] in 'FOUfou']
